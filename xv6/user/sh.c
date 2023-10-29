1a61b45e954bd0bcc8caf0e7e7234e37e57d5538
ioannisleo <trickle-takes06@icloud.com>
2023-10-29 03:36:32 +0300

    Change prompt

--- xv6/user/sh.c
+++ xv6/user/sh.c
@@ -129,7 +129,7 @@ void runcmd(struct cmd *cmd) {
 }
 
 int getcmd(char *buf, int nbuf) {
-    printf(2, "$ ");
+    printf(2, "➤ ");
     memset(buf, 0, nbuf);
     gets(buf, nbuf);
     if (buf[0] == 0) // EOF

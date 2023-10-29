75f7ab50e76e17ce88a3fa999eefde40a23925b4
ioannisleo <trickle-takes06@icloud.com>
2023-10-29 03:15:16 +0300

    Improve killrandom system call

--- activity-1/xv6/kernel/random.c
+++ activity-1/xv6/kernel/random.c
@@ -1,7 +1,8 @@
 static int next = 1046836;
 
-int random(int max)
-{
+int random(int max) {
+    if (max < 1)
+        return 0;
     next = next * 1103515245 + 12345;
-    return (unsigned int) (next / 65536) % max;
+    return (unsigned int)(next / 65536) % max;
 }
